590b2bf4038fe7dd329c1bf695f8510ebf882931
MaNa <andreygorlov19995@gmail.com>
2020-03-22 19:06:41 +0300

    2

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var canvas = document.getElementById("test"), 
 context = canvas.getContext("2d");
-var x=50;
-var x1=340;  
-var x2=630;  
-var x3=920;  
-var x4=50;
-var x5=340;  
-var x6=630;  
-var x7=920; 
-var x8=50;
-var x9=340;  
-var x10=630;  
-var x11=920; 
+var x= -140;
+var x1=150;  
+var x2=440;  
+var x3=730;  
+var x4=-140;
+var x5=150;   
+var x6=440;   
+var x7=730;  
+var x8= -140;
+var x9= 150;   
+var x10=440;   
+var x11=730;  
 var y=20;  
 var time=new Date();
 var s=0;
@@ -138,11 +138,11 @@ function changeSeconds(s){
 }
 
 function draw(){
-	gradient = context.createLinearGradient(800, 0, 450, 0);
+	gradient = context.createLinearGradient(500, 0, 450, 0);
 	context.beginPath();
 	context.moveTo(0, 0);
-	context.lineTo(900, 0);
-	context.lineTo(900, 720);
+	context.lineTo(500, 0);
+	context.lineTo(500, 720);
 	context.lineTo(0, 720);
 	context.closePath();
 	gradient.addColorStop(0, "BurlyWood");
@@ -150,18 +150,19 @@ function draw(){
 	gradient.addColorStop('0.7', "white");
 	gradient.addColorStop(1, "BurlyWood");
 	context.fillStyle = gradient;
-	context.fillRect(0, 0, 900, 720);
-	gradient = context.createLinearGradient(350, 0, 450, 0);
+	context.fillRect(0, 0, 500, 720);
+	gradient = context.createLinearGradient(200, 0, 300, 0);
 	gradient.addColorStop(1, "Red");
 	gradient.addColorStop(1, "Red");
+	
 	context.fillStyle = gradient;
-	context.fillRect(300, 0, 330, 720);
+	context.fillRect(140, 0, 210, 720);
 	context.stroke();
 	context.beginPath();
 	context.moveTo(0, 220);
-	context.lineTo(900, 220);
+	context.lineTo(500, 220);
 	context.moveTo(0, 470);
-	context.lineTo(900, 470);
+	context.lineTo(500, 470);
 	context.closePath();
 	context.stroke();
 	DrawNumber(s,x,y);
@@ -179,51 +180,51 @@ function draw(){
 	DrawNumber(s11,x11,y+500);
 	time=new Date();
 	console.log(time.getMinutes());
-	if(x<=-100){
+	if(x<=-200){
 		s=changeSeconds(s3+1);
 		x=x3+290;	
 	}
-	if(x1<=-100){
+	if(x1<=-200){
 		s1=changeSeconds(s+1);
 		x1=x+290;
 	}
-	if(x2<=-100){
+	if(x2<=-200){
 		s2=changeSeconds(s1+1);
 		x2=x1+290;
 	}
-	if(x3<=-100){
+	if(x3<=-200){
 		s3=changeSeconds(s2+1);
 		x3=x2+290;
 	}
-	if(x4<=-100){
+	if(x4<=-200){
 		s4=changeSeconds(s7+1);
 		x4=x7+290;	
 	}
-	if(x5<=-100){
+	if(x5<=-200){
 		s5=changeSeconds(s4+1);
 		x5=x4+290;
 	}
-	if(x6<=-100){
+	if(x6<=-200){
 		s6=changeSeconds(s5+1);
 		x6=x5+290;
 	}
-	if(x7<=-100){
+	if(x7<=-200){
 		s7=changeSeconds(s6+1);
 		x7=x6+290;
 	}
-	if(x8<=-100){
+	if(x8<=-200){
 		s8=changeSeconds(s11+1);
 		x8=x11+290;	
 	}
-	if(x9<=-100){
+	if(x9<=-200){
 		s9=changeSeconds(s8+1);
 		x9=x8+290;
 	}
-	if(x10<=-100){
+	if(x10<=-200){
 		s10=changeSeconds(s9+1);
 		x10=x9+290;
 	}
-	if(x11<=-100){
+	if(x11<=-200){
 		s11=changeSeconds(s10+1);
 		x11=x10+290;
 	}
@@ -319,10 +320,14 @@ function DrawNumber(date,x,y){
 function LoadPage(){
 	draw();
 	time=new Date();
-	s=changeSeconds(time.getSeconds()-1);
-	s1=changeSeconds(time.getSeconds());
-	s2=changeSeconds(time.getSeconds()+1);
-	s3=changeSeconds(time.getSeconds()+2);
+	//s=changeSeconds(time.getSeconds()-1);
+	//s1=changeSeconds(time.getSeconds());
+	//s2=changeSeconds(time.getSeconds()+1);
+	//s3=changeSeconds(time.getSeconds()+2);
+	s=changeSeconds(0-1);
+	s1=changeSeconds(0);
+	s2=changeSeconds(0+1);
+	s3=changeSeconds(0+2);
 	s4=changeSeconds(time.getMinutes()-1);
 	s5=changeSeconds(time.getMinutes());
 	s6=changeSeconds(time.getMinutes()+1);
